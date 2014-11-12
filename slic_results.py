16903050b8e5934ce1e59e91ef6b096683747d93
Gervase Markham <gerv@gerv.net>
2014-11-12 15:50:52 +0000

    Remove old "relic" script.

---
+++ slic_results.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+###############################################################################
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+###############################################################################
+#
+# The internal data structure here is a dict. The keys are Slic tag names,
+# which identify a license. The values are a list of one or more occurrences
+# of a bit of text which identifies that license. Each unique textual
+# representation becomes a different item in the list. Attached to the text is
+# a list of files where that text appeared, and a list of recognisable
+# copyright lines parsed from the comment in which the text appears.
+
+import json
+import re
+import itertools
+
+class SlicResults(dict):
+    def load_json(self, data):
+        """Populates the Results from a JSON file."""
+        self.__init__(json.loads(data))
+
+    def pop_by_re(self, regexps):
+        """Creates another SlicResults with all entries which match any of the
+        regexps given, and removes them from this one.
+        """
+        subset = SlicResults()
+        
+        if type(regexps) == str:
+            regexps = [regexps]
+
+        for regexp in regexps:
+            key_re = re.compile(regexp)
+            for k in self.keys():
+              if key_re.search(k):
+                subset[k] = self.pop(k)
+
+        return subset
+
+    def unify(self):
+        """Combines all of the items in the lists into a single item per tag.
+        It does this by combining the lists of copyright holders and the lists
+        of files, and taking the text from an (undefined) member of the set.
+        """
+        for tag, datalist in self.iteritems():        
+            license = {
+                'tag': tag,
+                'copyrights': [],
+                'files': [],
+                'text': None,
+            }
+
+            for data in datalist:
+                license['copyrights'].extend(data['copyrights'])
+                license['files'].extend(data['files'])
+                license['text'] = data['text']
+
+            self[tag] = [license]
+
+    def iterflatvalues(self, regexp=""):
+        """Returns an iterator which iterates over all items in the lists"""
+        tag_re = re.compile(regexp)
+        # Returns all members of all lists
+        # Takes optional regexp to match tags against
+        return itertools.chain(data for tag, data in self.iteritems()
+                                                         if tag_re.search(tag))
+ 
---
+++ test_slic_results.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+###############################################################################
+import config
+import os
+import re
+
+from nose.tools import *
+from license_data import license_data
+
+from slic_results import SlicResults
+
+class TestSlicResults():
+    def make_example(self):
+        data = """{
+    "BSD-4-Clause": [
+    {
+        "files": [ "foo.html" ], 
+        "text": [
+          "Redistribution and use in source and binary forms, with or without", 
+          "modification, are permitted provided that the following conditions", 
+          "are met:" ], 
+        "tag": "BSD-4-Clause", 
+        "copyrights": []
+    },
+    {
+        "files": [ "quux.html", "baz.txt" ], 
+        "text": [
+          "Redistribution and use in source and binary forms, with or without", 
+          "modification, are permitted provided that the following conditions", 
+          "are met:" ], 
+        "tag": "BSD-4-Clause", 
+        "copyrights": ["Copyright (C) 2014 Fred Bloggs"]
+    }],
+      "GPL-2.0": [{
+        "files": [ "bar.html" ], 
+        "text": [ "" ], 
+        "tag": "GPL-2.0", 
+        "copyrights": []
+      }]
+}"""
+        res = SlicResults()
+        res.load_json(data)
+        return res
+                
+    def test_creation(self):
+        res = self.make_example()
+        assert_equal(len(res), 2)
+        assert_equal(res['BSD-4-Clause'][0]['files'][0], "foo.html")
+
+    def test_unify(self):
+        res = self.make_example()
+        assert_equal(len(res['BSD-4-Clause']), 2)
+        res.unify()
+        assert_equal(len(res['BSD-4-Clause']), 1)
+        data = res['BSD-4-Clause'][0]
+        assert_equal(data['tag'], 'BSD-4-Clause')
+        assert_equal(len(data['files']), 3)
+        assert_equal(len(data['copyrights']), 1)
+
+    def test_pop_by_re(self):
+        res = self.make_example()
+        bsd = res.pop_by_re("^BSD")
+        assert_equal(len(bsd), 1)
+        assert_equal(len(bsd['BSD-4-Clause']), 2)
+        assert_equal(len(res), 1)
+        assert_equal(len(res['GPL-2.0']), 1)
+
+    def test_iterflatvalues(self):
+        res = self.make_example()
+        values = [item for item in res.iterflatvalues()]
+        assert_equal(len(values), 3)
+        
+if __name__ == '__main__':
+    nose.main()
